b2c1eec1715a03b7c19eebd2a390eb296ce8ccd4
Pete Slater <pslater@equalexperts.com>
2020-10-16 14:15:46 +0100

    APIS-4836 - Added test for subs service

--- test/uk/gov/hmrc/apiplatformmicroservice/thirdpartyapplication/services/SubscriptionServiceSpec.scala
+++ test/uk/gov/hmrc/apiplatformmicroservice/thirdpartyapplication/services/SubscriptionServiceSpec.scala
@@ -16,6 +16,73 @@
 
 package uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.services
 
-class SubscriptionServiceSpec {
-  // TODO: Test subscription service
+import uk.gov.hmrc.apiplatformmicroservice.util.AsyncHmrcSpec
+import uk.gov.hmrc.apiplatformmicroservice.apidefinition.services.ApiDefinitionsForApplicationFetcher
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import uk.gov.hmrc.http.HeaderCarrier
+import uk.gov.hmrc.apiplatformmicroservice.apidefinition.models.ApiIdentifier
+
+import scala.concurrent.Future.successful
+import uk.gov.hmrc.apiplatformmicroservice.apidefinition.models.ApiDefinitionTestDataHelper
+import uk.gov.hmrc.apiplatformmicroservice.common.builder.ApplicationBuilder
+import uk.gov.hmrc.apiplatformmicroservice.common.domain.models.ApplicationId
+import uk.gov.hmrc.apiplatformmicroservice.apidefinition.models.ApiContext
+import uk.gov.hmrc.apiplatformmicroservice.apidefinition.models.ApiVersion
+import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.services.SubscriptionService.CreateSubscriptionDuplicate
+import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.services.SubscriptionService.CreateSubscriptionDenied
+import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.services.SubscriptionService.CreateSubscriptionSuccess
+
+class SubscriptionServiceSpec extends AsyncHmrcSpec {
+  trait Setup extends ApplicationBuilder with ApiDefinitionTestDataHelper {
+    val mockApiDefinitionsForApplicationFetcher = mock[ApiDefinitionsForApplicationFetcher]
+    val underTest = new SubscriptionService(mockApiDefinitionsForApplicationFetcher)
+
+    implicit val hc = HeaderCarrier()
+    
+    val apiDefinitionOne = apiDefinition("one")
+    val apiDefinitionTwo = apiDefinition("two")
+    val apiDefinitionThree = apiDefinition("three")
+    val apiDefintions = Seq(apiDefinitionOne, apiDefinitionTwo, apiDefinitionThree)
+    when(mockApiDefinitionsForApplicationFetcher.fetchUnrestricted(*, *)(*)).thenReturn(successful(apiDefintions))
+
+    val apiVersionOne = ApiVersion("1.0")
+    val apiVersionTwo = ApiVersion("2.0")
+    val apiIdentifierOne = ApiIdentifier(apiDefinitionOne.context, apiVersionOne)
+    val apiIdentifierTwo = ApiIdentifier(apiDefinitionTwo.context, apiVersionOne)
+    val apiIdentifierThree = ApiIdentifier(apiDefinitionThree.context, apiVersionOne)
+
+    val applicationId = ApplicationId.random
+    val application = buildApplication(appId = applicationId)
+  }
+
+
+  "createSubscriptionForApplication" should {
+    "CreateSubscriptionDuplicate when tApiContexttion is already subscribed to the API " in new Setup {
+      val duplicateApi = apiIdentifierOne
+      val existingApiSubscriptions = Set(apiIdentifierOne, apiIdentifierTwo)
+
+      val result = await(underTest.createSubscriptionForApplication(application, existingApiSubscriptions, duplicateApi))
+
+      result shouldBe CreateSubscriptionDuplicate
+    }
+
+    "CreateSubscriptionDenied when the application cannot subscribe to the API " in new Setup {
+      val deniedApi = ApiIdentifier(apiDefinitionOne.context, apiVersionTwo)
+      val existingApiSubscriptions = Set(apiIdentifierOne, apiIdentifierTwo)
+
+      val result = await(underTest.createSubscriptionForApplication(application, existingApiSubscriptions, deniedApi))
+
+      result shouldBe CreateSubscriptionDenied
+    }
+
+    "CreateSubscriptionSuccess when successfully subscribing to API " in new Setup {
+      val goodApi = apiIdentifierThree
+      val existingApiSubscriptions = Set(apiIdentifierOne, apiIdentifierTwo)
+
+      val result = await(underTest.createSubscriptionForApplication(application, existingApiSubscriptions, goodApi))
+
+      result shouldBe CreateSubscriptionSuccess
+    }
+  }
 }
