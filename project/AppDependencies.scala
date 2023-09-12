c5bafe8b522fc3662828420bbe1fc7b7357e5d2d
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-09-12 14:26:54 +0100

    APIS-6414 - Test lib versions

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
   lazy val dependencies = Seq(
     caffeine,
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"         % bootstrapVersion,
-    "com.beachape"            %% "enumeratum-play-json"              % "1.6.0",
+    "com.beachape"            %% "enumeratum-play-json"              % "1.6.2",
     "org.typelevel"           %% "cats-core"                         % "2.6.1",
     "com.typesafe.play"       %% "play-json"                         % "2.9.2",
     "org.julienrf"            %% "play-json-derived-codecs"          % "7.0.0",
@@ -19,7 +19,7 @@ object AppDependencies {
   lazy val testDependencies = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % bootstrapVersion,
     "org.pegdown"             %  "pegdown"                    % "1.6.0",
-    "org.mockito"             %% "mockito-scala-scalatest"    % "1.7.1",
+    "org.mockito"             %% "mockito-scala-scalatest"    % "1.17.12",
     "com.github.tomakehurst"  %  "wiremock-jre8-standalone"   % "2.27.1"
   ).map(_ % "test,it")
 }
