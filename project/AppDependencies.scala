86901de0ea633d13526100eb8a3e6d83d63f0b58
Matt Clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2023-10-06 14:21:16 +0100

    APIS-6473: remove application-command library and replace with new domain library that includes commands

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "org.julienrf"            %% "play-json-derived-codecs"          % "7.0.0",
     "uk.gov.hmrc"             %% "json-encryption"                   % "5.1.0-play-28",
     "uk.gov.hmrc"             %% "api-platform-api-domain"           % "0.6.0",
-    "uk.gov.hmrc"             %% "api-platform-application-commands" % "0.28.0"
+    "uk.gov.hmrc"             %% "api-platform-application-domain"   % "0.21.0"
   )
 
   lazy val testDependencies = Seq(
