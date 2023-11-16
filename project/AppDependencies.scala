233809c351ec08e058d0547907348a1884a4d0fc
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2023-11-16 10:33:29 +0000

    APIS-6541-D - Bump lib version (#146)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "org.julienrf"            %% "play-json-derived-codecs"          % "10.0.2",
     "uk.gov.hmrc"             %% "json-encryption"                   % "5.1.0-play-28",
     "uk.gov.hmrc"             %% "api-platform-api-domain"           % "0.9.0",
-    "uk.gov.hmrc"             %% "api-platform-application-domain"   % "0.24.0"
+    "uk.gov.hmrc"             %% "api-platform-application-domain"   % "0.25.0"
   )
 
   lazy val testDependencies = Seq(
