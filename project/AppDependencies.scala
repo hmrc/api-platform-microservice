83116161daf201aed6f020c292b16ba96f5abd77
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2024-10-21 08:50:23 +0100

    API-7888 - Bump libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ import sbt._
 
 object AppDependencies {
   val bootstrapVersion = "9.3.0"
-  val apiDomainVersion = "0.17.0"  // 0.17.0 uses common-domain 0.13.0
-  val commonDomainVersion = "0.13.0"
-  val appDomainVersion = "0.61.7"
+  val apiDomainVersion = "0.19.1"
+  // val commonDomainVersion = "0.17.0"
+  val appDomainVersion = "0.61.16"
   val tpdDomainVersion = "0.10.0"
 
   lazy val dependencies = Seq(
