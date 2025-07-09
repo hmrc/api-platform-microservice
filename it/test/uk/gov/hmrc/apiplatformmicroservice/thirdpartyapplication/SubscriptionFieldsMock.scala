6470ef40152827d208be6f8b7a516afaa6e8bca1
peteslater-ee <28756214+peteslater-ee@users.noreply.github.com>
2025-07-09 13:50:55 +0100

    API-8479 - Adding more tests

--- it/test/uk/gov/hmrc/apiplatformmicroservice/thirdpartyapplication/SubscriptionFieldsMock.scala
+++ it/test/uk/gov/hmrc/apiplatformmicroservice/thirdpartyapplication/SubscriptionFieldsMock.scala
@@ -19,15 +19,27 @@ package uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication
 import com.github.tomakehurst.wiremock.client.WireMock._
 
 import play.api.http.Status._
-import uk.gov.hmrc.apiplatform.modules.common.domain.models._
-import uk.gov.hmrc.apiplatformmicroservice.utils.PrincipalAndSubordinateWireMockSetup
-import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.controllers.SubscriptionFieldsController.Fields
+import play.api.http.{HeaderNames, MimeTypes}
+import play.api.libs.json.Json
 
-trait SubscriptionFieldsMock {
+import uk.gov.hmrc.apiplatform.modules.common.domain.models._
+import uk.gov.hmrc.apiplatform.modules.subscriptionfields.domain.models.{FieldErrorMap, FieldNameFixtures, FieldsFixtures}
+import uk.gov.hmrc.apiplatformmicroservice.utils.PrincipalAndSubordinateWireMockSetup
+
+trait SubscriptionFieldsMock extends FieldsFixtures with FieldNameFixtures {
   self: PrincipalAndSubordinateWireMockSetup => // To allow for stubFor to work with environment
 
+  def mockUpsert(env: Environment): Unit = {
+    stubFor(env)(put(urlMatching(""".*/field/application/.*"""))
+      .withRequestBody(matchingJsonPath("$.fields"))
+      .willReturn(
+        aResponse()
+          .withStatus(OK)
+      ))
+  }
+
   def mockUpsertNotFound(env: Environment): Unit = {
-    stubFor(env)(put(urlMatching("""/field/application/[^/]+/context/[^/]+/version/[^/]+"""))
+    stubFor(env)(put(urlMatching(""".*/field/application/.*/context/.*/version/.*+"""))
       .withRequestBody(matchingJsonPath("$.fields"))
       .willReturn(
         aResponse()
@@ -35,14 +47,16 @@ trait SubscriptionFieldsMock {
       ))
   }
 
-  // def mockUpsert(deployedTo: Environment, fields: Fields): Unit = {
-  //   stubFor(deployedTo)(get(urlEqualTo(s"/developer/$userId/applications"))
-  //     .willReturn(
-  //       aResponse()
-  //         .withBody(Json.toJson(
-  //         ).toString())
-  //         .withHeader(HeaderNames.CONTENT_TYPE, MimeTypes.JSON)
-  //         .withStatus(OK)
-  //     ))
-  // }
+  def mockUpsertWithBadRequest(env: Environment): Unit = {
+    val errs: FieldErrorMap = Map(fieldNameOne -> "Error1")
+    stubFor(env)(put(urlMatching(""".*/field/application/.*/context/.*/version/.*+"""))
+      .withRequestBody(matchingJsonPath("$.fields"))
+      .willReturn(
+        aResponse()
+          .withBody(Json.toJson(errs).toString())
+          .withHeader(HeaderNames.CONTENT_TYPE, MimeTypes.JSON)
+          .withStatus(BAD_REQUEST)
+      ))
+  }
+
 }
--- it/test/uk/gov/hmrc/apiplatformmicroservice/thirdpartyapplication/controllers/SubscriptionFieldsControllerSpec.scala
+++ it/test/uk/gov/hmrc/apiplatformmicroservice/thirdpartyapplication/controllers/SubscriptionFieldsControllerSpec.scala
@@ -16,28 +16,48 @@
 
 package uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.controllers
 
-import uk.gov.hmrc.apiplatformmicroservice.utils._
-import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.SubscriptionFieldsMock
 import play.api.http.HeaderNames._
 import play.api.http.MimeTypes._
 import play.api.http.Status._
 import play.api.libs.ws.WSClient
+
 import uk.gov.hmrc.apiplatform.modules.common.domain.models._
-import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.controllers.SubscriptionFieldsController.SubscriptionFieldsRequest
+import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.SubscriptionFieldsMock
+import uk.gov.hmrc.apiplatformmicroservice.utils._
 
 class SubscriptionFieldsControllerSpec extends WireMockSpec with SubscriptionFieldsMock with ApiIdentifierFixtures {
-  
-  "SubsFieldsController" should {
+
+  "SubscriptionFieldsController" should {
     val wsClient = app.injector.instanceOf[WSClient]
 
-    "post resulting in not found in back end" in {
+    "put resulting in NOT FOUND in back end" in {
       mockUpsertNotFound(Environment.PRODUCTION)
-      
-      val response = await(wsClient.url(s"$baseUrl/field/application/${ClientId.random}/context/$apiContextOne/version/$apiVersionNbrOne")
-        .withHttpHeaders(ACCEPT -> JSON)
-        .post("""{ "fields": {} }"""))
+
+      val response = await(wsClient.url(s"$baseUrl/subscription-fields/field/application/xyz/context/abc/version/stu?environment=PRODUCTION")
+        .withHttpHeaders(ACCEPT -> JSON, CONTENT_TYPE -> JSON)
+        .put("""{ "fields": { "A" : "x" } }"""))
 
       response.status shouldBe NOT_FOUND
+    }
+
+    "put resulting in BAD REQUEST when back end responds with BAD REQUEST" in {
+      mockUpsertWithBadRequest(Environment.PRODUCTION)
+
+      val response = await(wsClient.url(s"$baseUrl/subscription-fields/field/application/xyz/context/abc/version/stu?environment=PRODUCTION")
+        .withHttpHeaders(ACCEPT -> JSON, CONTENT_TYPE -> JSON)
+        .put("""{ "fields": { "A" : "x" } }"""))
+
+      response.status shouldBe BAD_REQUEST
+    }
+
+    "put resulting in OK when back end responds with OK" in {
+      mockUpsert(Environment.PRODUCTION)
+
+      val response = await(wsClient.url(s"$baseUrl/subscription-fields/field/application/xyz/context/abc/version/stu?environment=PRODUCTION")
+        .withHttpHeaders(ACCEPT -> JSON, CONTENT_TYPE -> JSON)
+        .put("""{ "fields": { "A" : "x" } }"""))
+
+      response.status shouldBe OK
     }
   }
 }
