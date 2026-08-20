7c903cd2dd61bdeb864c2a60b0b49b2c5c08ef23
Andy Spaven <16537059+AndySpaven@users.noreply.github.com>
2026-08-20 07:46:43 +0100

    API-9363 - Bump libs

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,15 +3,18 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootstrapVersion = "10.7.1"
-  val apiDomainVersion = "1.5.0"
-  val appDomainVersion = "1.3.0"
-  val tpdDomainVersion = "1.0.0"
-  val mockitoScalaVersion = "2.2.1"
+  private val bootstrapVersion = "10.7.1"
+  private val mockitoScalaVersion = "2.2.1"
+
+  private val commonDomainVersion    = "1.4.0"
+  private val apiDomainVersion       = "1.8.0"
+  private val appDomainVersion       = "1.6.0"
+  private val tpdDomainVersion       = "1.3.0"
 
   lazy val dependencies = Seq(
     caffeine,
     "uk.gov.hmrc"             %% "bootstrap-backend-play-30"         % bootstrapVersion,
+    "uk.gov.hmrc"             %% "api-platform-common-domain"        % commonDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-api-domain"           % apiDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-application-domain"   % appDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-tpd-domain"           % tpdDomainVersion
@@ -20,7 +23,8 @@ object AppDependencies {
   lazy val testDependencies = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"                     % bootstrapVersion,
     "org.mockito"             %% "mockito-scala-scalatest"                    % mockitoScalaVersion,
-    "uk.gov.hmrc"             %% "api-platform-test-tpd-domain"               % tpdDomainVersion,
+    "uk.gov.hmrc"             %% "api-platform-common-domain-fixtures"        % tpdDomainVersion,
+    "uk.gov.hmrc"             %% "api-platform-tpd-domain-fixtures"           % tpdDomainVersion,
     "uk.gov.hmrc"             %% "api-platform-application-domain-fixtures"   % appDomainVersion
   ).map(_ % "test")
 }
