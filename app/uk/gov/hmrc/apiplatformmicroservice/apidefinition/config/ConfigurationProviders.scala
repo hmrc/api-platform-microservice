8f1ac66d204063f2ba33c0147b7d0d773b7426f7
John Green <john.green@digital.hmrc.gov.uk>
2026-09-11 14:11:24 +0100

    API-9494 Adding Advanced User (#202)
    * API-9494 Adding Advanced User
    
    * API-9494 Move roles under 'stride' in config
    
    * API-9494 Remove incorrect space

--- app/uk/gov/hmrc/apiplatformmicroservice/apidefinition/config/ConfigurationProviders.scala
+++ app/uk/gov/hmrc/apiplatformmicroservice/apidefinition/config/ConfigurationProviders.scala
@@ -95,13 +95,15 @@ class AuthConfigProvider @Inject() (val configuration: Configuration)
 
   override def get() = {
     val url              = baseUrl("auth")
-    val userRole         = getString("roles.user")
-    val superUserRole    = getString("roles.super-user")
-    val adminRole        = getString("roles.admin")
+    val strideConfig     = configuration.underlying.getConfig("stride")
+    val userRole         = strideConfig.getString("roles.user")
+    val advancedUserRole = strideConfig.getString("roles.advanced-user")
+    val superUserRole    = strideConfig.getString("roles.super-user")
+    val adminRole        = strideConfig.getString("roles.admin")
     val enabled          = getConfBool("auth.enabled", true)
     val authorisationKey = getString("authorisationKey")
 
-    AuthConnector.Config(url, userRole, superUserRole, adminRole, enabled, authorisationKey)
+    AuthConnector.Config(url, userRole, advancedUserRole, superUserRole, adminRole, enabled, authorisationKey)
   }
 }
 
