319f24c6678f633c1721f64ebdcf4c75d9d59827
John Green <john.green@digital.hmrc.gov.uk>
2026-09-24 17:08:00 +0100

    API-8699 - Adding some tests

--- test/uk/gov/hmrc/apiplatformmicroservice/apidefinition/controllers/ApiDefinitionControllerSpec.scala
+++ test/uk/gov/hmrc/apiplatformmicroservice/apidefinition/controllers/ApiDefinitionControllerSpec.scala
@@ -1,25 +1,45 @@
+/*
+ * Copyright 2026 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.apiplatformmicroservice.apidefinition.controllers
 
-import uk.gov.hmrc.http.HeaderCarrier
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.stream.Materializer
-import uk.gov.hmrc.apiplatformmicroservice.common.utils.AsyncHmrcSpec
-import uk.gov.hmrc.apiplatformmicroservice.apidefinition.models.ApiDefinitionTestDataHelper
-import play.api.libs.ws.{JsonBodyWritables, JsonBodyReadables}
-import org.apache.pekko.stream.testkit.NoMaterializer
+
+import play.api.libs.ws.{JsonBodyReadables, JsonBodyWritables}
 import play.api.test.FakeRequest
+import play.api.test.Helpers.*
+import uk.gov.hmrc.http.{HeaderCarrier, HttpResponse}
+
 import uk.gov.hmrc.apiplatform.modules.common.domain.models.*
 import uk.gov.hmrc.apiplatform.modules.apis.domain.models.ServiceName
-import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.services.ApplicationByIdFetcher
+import uk.gov.hmrc.apiplatformmicroservice.apidefinition.mocks.ApiDefinitionServiceModule
+import uk.gov.hmrc.apiplatformmicroservice.apidefinition.models.{ApiDefinitionTestDataHelper, ResourceId}
 import uk.gov.hmrc.apiplatformmicroservice.apidefinition.services.*
 import uk.gov.hmrc.apiplatformmicroservice.common.connectors.AuthConnector
-import play.api.test.Helpers
-import scala.concurrent.ExecutionContext.Implicits.global
+import uk.gov.hmrc.apiplatformmicroservice.common.utils.AsyncHmrcSpec
+import uk.gov.hmrc.apiplatformmicroservice.thirdpartyapplication.services.ApplicationByIdFetcher
 
 class ApiDefinitionControllerSpec extends AsyncHmrcSpec with ApiDefinitionTestDataHelper with JsonBodyWritables with JsonBodyReadables {
 
-  trait Setup {
+  trait Setup extends ApiDefinitionServiceModule {
     implicit val headerCarrier: HeaderCarrier = HeaderCarrier()
-    implicit val mat: Materializer            = NoMaterializer
+    implicit val actorSystem: ActorSystem     = ActorSystem("test") // NoMaterializer does not work with streamed responses
 
     val request                 = FakeRequest("GET", "/")
     val apiName                 = "hello-api"
@@ -27,28 +47,42 @@ class ApiDefinitionControllerSpec extends AsyncHmrcSpec with ApiDefinitionTestDa
     val version                 = ApiVersionNbr("1.0")
     val anApiDefinition         = apiDefinition(apiName)
     val anExtendedApiDefinition = extendedApiDefinition(apiName)
+    val resource                = "application.yml"
+    val resourceId              = ResourceId(serviceName, version, resource)
 
     val controller = new ApiDefinitionController(
       mock[ApplicationByIdFetcher],
       mock[ApiDefinitionsForApplicationFetcher],
-      mock[EnvironmentAwareApiDefinitionService],
+      EnvironmentAwareApiDefinitionServiceMock.instance,
       mock[OpenAccessApisFetcher],
       mock[ApisFetcher],
       mock[AuthConnector.Config],
       mock[AuthConnector],
-      Helpers.stubControllerComponents(),
+      stubControllerComponents(),
       mock[ApiIdentifiersForUpliftFetcher],
       mock[ApiEventsFetcher]
     )
-    
-    // val mockHttpResponse = mock[HttpResponse]
-    // when(mockHttpResponse.status).thenReturn(OK)
-    // when(mockHttpResponse.headers).thenReturn(
-    //   Map(
-    //     "Content-Length" -> Seq("500")
-    //   )
-    // )
-    // when(mockHttpResponse.header(eqTo(PROXY_SAFE_CONTENT_TYPE))).thenReturn(None)
-    // when(mockHttpResponse.header(eqTo(HeaderNames.CONTENT_TYPE))).thenReturn(Some("application/json"))
+  }
+
+  "fetchApiDocumentationResource" should {
+    "return a resource from production" in new Setup {
+      val body = "some yaml"
+      PrincipalApiDefinitionServiceMock.FetchApiDocumentationResource.willReturnWsResponseFor(resourceId, HttpResponse(OK, body))
+
+      val result = controller.fetchApiDocumentationResource(Environment.Production, serviceName, version, resource)(request)
+
+      status(result) shouldBe OK
+      contentAsString(result) shouldBe body
+    }
+
+    "return a resource from sandbox" in new Setup {
+      val body = "some yaml"
+      SubordinateApiDefinitionServiceMock.FetchApiDocumentationResource.willReturnWsResponseFor(resourceId, HttpResponse(OK, body))
+
+      val result = controller.fetchApiDocumentationResource(Environment.Sandbox, serviceName, version, resource)(request)
+
+      status(result) shouldBe OK
+      contentAsString(result) shouldBe body
+    }
   }
 }
